364a39f07e8d8024184f8507ef4c4e3a09590eb3
Andrianna Yovbak <37146638+AndryushkaYO@users.noreply.github.com>
2019-02-05 20:23:45 +0200

    6

---
+++ FLX_homework_6/homework/js/task1.js
@@ -0,0 +1,21 @@
+var a = prompt("Enter a: ");
+var b = prompt("Enter b: ");
+var c = prompt("Enter c: ");
+
+if (isNaN(a) || isNaN(b) || isNaN(c) ||
+    (a === "") || (b === "") || (c === "")||+a===0) {
+    alert("Invalid input data");
+} else {
+    var x = 0;
+    var discriminant = (b * b) - 4 * a * c;
+    if (discriminant < 0) {
+        alert("no solution");
+    } else if (discriminant > 0) {
+        x = ((-b) - Math.sqrt(discriminant)) / (2 * a);
+        var x2 = ((-b) + Math.sqrt(discriminant)) / (2 * a);
+        alert("x1 = " + x + " and x2 = " + x2);
+    } else {
+        x = (-b) / (2 * a);
+        alert("x = " + x);
+    }
+}
